63d40dd670aca92d8f8208151e423a16adb2acef
Rushi-Technologies <rushitechnologiesbanglore@gmail.com>
2025-09-02 10:09:31 +0530

    Updated Module Code

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_route_table" "private" {
   count  = var.create_nat_gateway ? 1 : 0
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
   tags = merge({Name = "${var.name}-private-rt-${count.index + 1}"}, var.tags)
